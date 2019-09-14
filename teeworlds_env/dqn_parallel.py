48ae4877987fa3bd1498af2f8f1d830bcb84eaf1
Bluemi <bruno.schilling@protonmail.ch>
2019-09-14 12:45:28 +0200

    COLLECT_EXPERIENCE_SIZE=5000; REPLAY_SIZE=100000

--- teeworlds_env/dqn_parallel.py
+++ teeworlds_env/dqn_parallel.py
@@ -20,8 +20,8 @@ from utils import ExperienceBuffer, ACTIONS, ACTION_LABELS, Experience, load_con
 
 
 # exp collecting
-NUM_WORKERS = 4
-COLLECT_EXPERIENCE_SIZE = 2000  # init: 2000 (amount of experiences to collect after each training step)
+NUM_WORKERS = 6
+COLLECT_EXPERIENCE_SIZE = 5000  # init: 2000 (amount of experiences to collect after each training step)
 GAME_TICK_SPEED = 200  # default: 50 (game speed, when higher more screenshots needs to be captures)
 EPISODE_DURATION = 40  # default: 40
 MONITOR_WIDTH = 84  # init: 84 width of game screen
@@ -46,13 +46,13 @@ EPSILON_DECAY = 0.01  # init: 0.01
 
 # training
 REPLAY_START_SIZE = 4000  # init: 10000 (min amount of experiences in replay buffer before training starts)
-REPLAY_SIZE = 10000  # init: 10000 (max capacity of replay buffer)
+REPLAY_SIZE = 100000  # init: 10000 (max capacity of replay buffer)
 DEVICE = 'cuda'  # init: 'cpu'
 BATCH_SIZE = 512  # init: 32 (sample size of experiences from replay buffer)
 NUM_TRAININGS_PER_EPOCH = 50  # init: 50 (amount of BATCH_SIZE x NUM_TRAININGS_PER_EPOCH will be trained)
-GAMMA = 0.90  # init: .99 (bellman equation)
+GAMMA = 0.99  # init: .99 (bellman equation)
 LEARNING_RATE = 1e-4  # init: 1e-4 (also quite low eventually using default 1e-3)
-SYNC_TARGET_FRAMES = COLLECT_EXPERIENCE_SIZE * 5  # init: 1000 (how frequently we sync target net with net)
+SYNC_TARGET_FRAMES = COLLECT_EXPERIENCE_SIZE * 2  # init: 1000 (how frequently we sync target net with net)
 MAP_NAMES = ['newlevel_0', 'newlevel_1', 'newlevel_2', 'newlevel_3']
 
 # evaluation
