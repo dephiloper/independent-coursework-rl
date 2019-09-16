cafa31db34654023970c6187abcefa3f83803462
Bluemi <bruno.schilling@protonmail.ch>
2019-09-16 21:23:47 +0200

    GAME_TICK_SPEED=100; NUM_WORKERS=3; -MAPS[newlevel_3]

--- teeworlds_env/dqn_parallel.py
+++ teeworlds_env/dqn_parallel.py
@@ -20,9 +20,9 @@ from utils import ExperienceBuffer, ACTIONS, ACTION_LABELS, Experience, load_con
 
 
 # exp collecting
-NUM_WORKERS = 4
+NUM_WORKERS = 3
 COLLECT_EXPERIENCE_SIZE = 2000  # init: 2000 (amount of experiences to collect after each training step)
-GAME_TICK_SPEED = 200  # default: 50 (game speed, when higher more screenshots needs to be captures)
+GAME_TICK_SPEED = 100  # default: 50 (game speed, when higher more screenshots needs to be captures)
 EPISODE_DURATION = 40  # default: 40
 MONITOR_WIDTH = 84  # init: 84 width of game screen
 MONITOR_HEIGHT = 84  # init: 84 height of game screen (important for conv)
@@ -53,7 +53,7 @@ NUM_TRAININGS_PER_EPOCH = 50  # init: 50 (amount of BATCH_SIZE x NUM_TRAININGS_P
 GAMMA = 0.99  # init: .99 (bellman equation)
 LEARNING_RATE = 1e-4  # init: 1e-4 (also quite low eventually using default 1e-3)
 SYNC_TARGET_FRAMES = COLLECT_EXPERIENCE_SIZE * 5  # init: 10000 (how frequently we sync target net with net)
-MAP_NAMES = ['newlevel_0', 'newlevel_1', 'newlevel_2', 'newlevel_3']
+MAP_NAMES = ['newlevel_0', 'newlevel_1', 'newlevel_2']
 
 # evaluation
 STATES_TO_EVALUATE = 1000
